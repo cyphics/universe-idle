dda4ee8c0bb3df08fe75cbee02d1955908484912
cyphix <cyphix@cyphix.org>
2017-12-24 01:57:39 +0100

    implement operators for PhysicalUnit class

--- src/class/PhysicalUnit.cpp
+++ src/class/PhysicalUnit.cpp
@@ -22,68 +22,34 @@ BigNum PhysicalUnit::get_numerical_value() const{
   return _numerical_value;
 }
 
-bool operator ==(const PhysicalUnit& first, const PhysicalUnit& second) {
-  return first.get_numerical_value() == second.get_numerical_value();
-}
-
-/**
- * Overlad != operator
- */
-bool operator !=(const PhysicalUnit& first, const PhysicalUnit& second) {
-    return !(first == second);
-}
-
 /**
  * Overload << operator
  */
-std::ostream& operator<<(std::ostream& os, const PhysicalUnit& num){
-    return os <<  num.to_string();
+
+
+PhysicalUnit& PhysicalUnit::operator=(const PhysicalUnit& rhs){
+  _numerical_value = rhs.get_numerical_value();
+  return *this;
 }
 
-PhysicalUnit operator +(const PhysicalUnit& a, const PhysicalUnit& b){
-  return PhysicalUnit(a.get_numerical_value() + b.get_numerical_value());
+PhysicalUnit& PhysicalUnit::operator+=(const PhysicalUnit& rhs){
+  _numerical_value += rhs.get_numerical_value();
+  return *this;
 }
 
-void PhysicalUnit::operator +=(const PhysicalUnit& b){
-  this->_numerical_value += b.get_numerical_value();
+PhysicalUnit& PhysicalUnit::operator-=(const PhysicalUnit& rhs){
+  _numerical_value -= rhs.get_numerical_value();
+  return *this;
 }
 
-PhysicalUnit operator -(const PhysicalUnit& a, const PhysicalUnit& b){
-  return PhysicalUnit(a.get_numerical_value() - b.get_numerical_value());
-}
 
-void PhysicalUnit::operator -=(const PhysicalUnit& b){
-  this->_numerical_value -= b.get_numerical_value();
-}
 
-PhysicalUnit operator *(const PhysicalUnit& a, const PhysicalUnit& b){
-  return PhysicalUnit(a.get_numerical_value() * b.get_numerical_value());
-}
 
-PhysicalUnit operator *(const PhysicalUnit& a, const int& b){
-  return PhysicalUnit(a.get_numerical_value() * b);
-}
-
-PhysicalUnit operator /(const PhysicalUnit& a, const PhysicalUnit& b){
-  return PhysicalUnit(a.get_numerical_value() / b.get_numerical_value());
-}
-
-bool operator <(const PhysicalUnit& a, const PhysicalUnit& b){
-  return a.get_numerical_value() < b.get_numerical_value();
-}
-
-bool operator >(const PhysicalUnit& a, const PhysicalUnit& b){
-  return a.get_numerical_value() < b.get_numerical_value();
-}
-
-bool operator >=(const PhysicalUnit& a, const PhysicalUnit& b){
-  return a.get_numerical_value() >= b.get_numerical_value();
-}
-
-bool operator <=(const PhysicalUnit& a, const PhysicalUnit& b){
-  return a.get_numerical_value() <= b.get_numerical_value();
-}
 
 std::string PhysicalUnit::to_string() const{
   return _numerical_value.to_string();
+}
+
+std::ostream& operator<<(std::ostream& os, const PhysicalUnit& num){
+  return os <<  num.to_string();
 }
