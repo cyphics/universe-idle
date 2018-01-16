6f77bec66db8bb2061ab767203b38daab2bf6d64
cyphix <cyphix@cyphix.org>
2018-01-16 14:13:30 +0100

    add files

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,22 @@
-// -*- compile-command: "./../compile.sh"; -*-
-
-
 #include "../include/helpers/run_simulator.h"
 #include "../include/class/BigNum.h"
+#include "../include/class/Upgrade.h"
+#include "../include/helpers/upgrades_helper.h"
 
 int main(int argc, char *argv[])
 {
 
-  BigNum a = BigNum(1);
-  BigNum b(200.2242434951);
-  a += b;
+  UpgradeCostTableElement a_cost;
+  a_cost.initial_cost = BigNum(10);
+  a_cost.resource = Resource_ID::cinetic_energy;
 
-  std::cout << a  << "\n";
+  std::vector<UpgradeCostTableElement> list_of_cost;
+  list_of_cost.push_back(a_cost);
+  Upgrade an_upgrade = Upgrade(Upgrade_ID::small_boost, list_of_cost, 1.2);
 
-  //  run_simulator(duration, Strategy_ID::broken);
+  std::cout << an_upgrade.get_cost_given_level(1).to_string()  << "\n";
+  Time duration(40);
+  run_simulator(duration, Strategy_ID::broken);
 
   return 0;
 }
---
+++ test/src/Upgrade_tests.cpp
@@ -0,0 +1,23 @@
+// Upgrade_tests.cpp
+//
+// last-edit-by: <>
+//
+// Description:
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "catch.hpp"
+#include "../../include/class/Upgrade.h"
+#include "../../include/class/UpgradesList.h"
+#include "../../include/helpers/Initiate.h"
+
+TEST_CASE( "Upgrade initialization" ) {
+
+
+
+}
+
+
+//////////////////////////////////////////////////////////////////////
+// $Log:$
+//
