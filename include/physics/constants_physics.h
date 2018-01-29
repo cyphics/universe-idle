0a2606e123c9f236b387a71a4a541f154a99a27c
cyphics <cyphics@protonmail.com>
2018-01-29 15:41:38 +0100

    Change constant physics to const

--- include/physics/constants_physics.h
+++ include/physics/constants_physics.h
@@ -28,27 +28,26 @@ enum class Time{second, minute, hour, day, year};
 namespace Units{
 
 // Distance unit definitions
-static Distance meter = Distance(BigNum(1, 18));
-static Distance kilometer = meter * 1000;
-//static Distance astro_unit = meter * 149597870700;
-static Distance astro_unit = kilometer * 14959787.07;
-//static Distance light_year = kilometer * 9460730472580;
-static Distance light_year = astro_unit * 63241;
+const static Distance meter = Distance(BigNum(1, 18));
+const static Distance kilometer = meter * 1000;
+const static Distance astro_unit = kilometer * 14959787.07;
+const static Distance light_year = astro_unit * 63241;
 //static Distance parsec = kilometer * 30856775814671;
-//static Distance parsec = kilometer * 30856775814671;
-static Distance hubble_length = light_year * 14.4;
+const static Distance hubble_length = light_year * 14.4;
 
 // Some other important distances
-static Distance diameter_galaxy = light_year * 30000;
-static Distance space_between_galaxies = light_year * 3000000; // 3 million
-static Distance diameter_universe = light_year * 91000000000000.0; // 91 billion
+const static Distance nanometer = meter * pow(10, -9);
+const static Distance angstrom = meter * pow(10, -10);
+const static Distance diameter_galaxy = light_year * 30000;
+const static Distance space_between_galaxies = light_year * 3000000; // 3 million
+const static Distance diameter_universe = light_year * 91000000000000.0; // 91 billion
 
-static Time second = Time(1);
-static Time minute = second * 60;
-static Time hour = minute * 60;
-static Time day = hour * 24;
-static Time year = day * 365;
-static Time galactical_year = year * 250000000;
+const static Time second = Time(1);
+const static Time minute = second * 60;
+const static Time hour = minute * 60;
+const static Time day = hour * 24;
+const static Time year = day * 365;
+const static Time galactical_year = year * 250000000;
 
 }
 
